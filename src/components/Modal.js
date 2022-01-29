475c6fa51a7ab88062fd9fe2f1435e7003a4ee7a
Iulian-U <julianursu18@gmail.com>
2022-01-29 23:42:52 +0200

    code review

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,19 @@
+import "../style/css/modal.css";
 import React from "react";
 
-export default function Modal() {
-  return <div></div>;
+export default function Modal({ title, handleClose }) {
+  return (
+    <div className="modal_backdrop">
+      <div className="modal">
+        <div className="modal_header">
+          <div className="modal_title"> {title}</div>
+          <button className="close_modal_btn" onClick={handleClose}>
+            X
+          </button>
+        </div>
+        <hr />
+        <div className="modal_content"></div>
+      </div>
+    </div>
+  );
 }
--- src/components/appointments.js
+++ src/components/appointments.js
@@ -1,14 +1,25 @@
 import "../style/css/appointments.css";
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../components/Modal";
 
-export default function appointments() {
+export default function Appointments() {
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="appointments_component_container">
       <div className="btn_container">
-        <button>Add Appointment</button>
+        <button onClick={() => setShowModal(true)}>Add Appointment</button>
       </div>
       <hr />
       <div className="appointments_container"></div>
+      {showModal && (
+        <Modal title="Add Appointment" handleClose={handleClose}>
+          {" "}
+        </Modal>
+      )}
     </div>
   );
 }
